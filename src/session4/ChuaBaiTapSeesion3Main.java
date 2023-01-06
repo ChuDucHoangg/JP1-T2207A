ac39b8cb70feccb9ffc1b2db0861d6c0d5f6e629
ChuDucHoangg <110522309+ChuDucHoangg@users.noreply.github.com>
2023-01-06 16:16:37 +0700

    JP1-BUOI-4

--- src/session4/ChuaBaiTapSeesion3Main.java
+++ src/session4/ChuaBaiTapSeesion3Main.java
@@ -1,12 +1,16 @@
 package session4;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class ChuaBaiTapSeesion3Main {
     public static void main(String[] args) {
-        ChuaBaiTapSeession3[] hs = new ChuaBaiTapSeession3[10];
+//        ChuaBaiTapSeession3[] hs = new ChuaBaiTapSeession3[10];
+        ArrayList<ChuaBaiTapSeession3> hs = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < hs.length; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("Nhap ten:");
             String ten = sc.nextLine();
             System.out.println("Nhap ngay sinh:");
@@ -17,31 +21,38 @@ public class ChuaBaiTapSeesion3Main {
             String lh = sc.nextLine();
             System.out.println("diem trung binh:");
             double tb = sc.nextDouble();
-            hs[i] = new ChuaBaiTapSeession3(ten, ns, dc, lh, tb);
+            hs.add(new ChuaBaiTapSeession3(ten, ns, dc, lh, tb));
         }
 
-        for (int i = 0; i < hs.length - 1; i++) {
-            for (int j = 0; j < hs.length - i - 1; j++) {
-                if (hs[j].getTrungBinh() < hs[j + 1].getTrungBinh()) {
-                    ChuaBaiTapSeession3 tmp;
-                    tmp = hs[j];
-                    hs[j] = hs[j + 1];
-                    hs[j + 1] = tmp;
-                }
+//        for (int i = 0; i < hs.length - 1; i++) {
+//            for (int j = 0; j < hs.length - i - 1; j++) {
+//                if (hs[j].getTrungBinh() < hs[j + 1].getTrungBinh()) {
+//                    ChuaBaiTapSeession3 tmp;
+//                    tmp = hs[j];
+//                    hs[j] = hs[j + 1];
+//                    hs[j + 1] = tmp;
+//                }
+//            }
+//        }
+//
+//        for (int i = 0; i < hs.length; i++) {
+//            hs[i].inThongTin();
+//        }
+//        //cao nhat: hs[0]
+//        //thap nhat: hs[hs.legnth-1]
+//
+//        //xuat sac:
+//        for (int i = 0; i < hs.length; i++) {
+//            if (hs[i].getTrungBinh() >= 9.5) {
+//                hs[i].inThongTin();
+//            }
+//        }
+
+        Collections.sort(hs, new Comparator<ChuaBaiTapSeession3>() {
+            @Override
+            public  int compare(ChuaBaiTapSeession3 o1, ChuaBaiTapSeession3 o2) {
+                return o1.getTrungBinh() - o2.getTrungBinh() > 0? 0:-1;
             }
-        }
-
-        for (int i = 0; i < hs.length; i++) {
-            hs[i].inThongTin();
-        }
-        //cao nhat: hs[0]
-        //thap nhat: hs[hs.legnth-1]
-
-        //xuat sac:
-        for (int i = 0; i < hs.length; i++) {
-            if (hs[i].getTrungBinh() >= 9.5) {
-                hs[i].inThongTin();
-            }
-        }
+        });
     }
 }
---
+++ src/session4/MainStudentss3.java
@@ -0,0 +1,24 @@
+package session4;
+
+import  session4.ChuaBaiTapSeession3;
+
+import java.util.ArrayList;
+
+public class MainStudentss3 {
+    public static void main(String[] args) {
+        ChuaBaiTapSeession3[] hs = new ChuaBaiTapSeession3[10];
+
+        ArrayList<ChuaBaiTapSeession3> ls = new ArrayList<>();
+//        ls.add(new ChuaBaiTapSeession3());
+        ls.add(new ChuaBaiTapSeession3("A","20/1/2004","hanoi","t2207a",8.5));
+
+//        in thong tin sv1
+        ls.get(0).inThongTin();
+
+        ArrayList<String> strs = new ArrayList<>();
+
+//        ArrayList<Integer>
+    }
+
+
+}
