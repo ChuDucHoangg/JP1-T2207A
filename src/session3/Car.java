b68b1fa800763317d7e4a9a3b375769b02fc08cd
ChuDucHoangg <110522309+ChuDucHoangg@users.noreply.github.com>
2023-01-06 16:14:26 +0700

    JP1-BUOI-3

--- src/session3/Car.java
+++ src/session3/Car.java
@@ -3,17 +3,45 @@ package session3;
 public class Car {
     private  String brand;
     private  String name;
+    private double price;
 
     public Car() {
     }
 
-    public Car(String brand,String name){
+    public Car(String brand,String name,double price){
         this.brand = brand;
         this.name = name;
+        this.price = price;
     }
 
     public String getBrand() {
         return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void inCar(){
+        System.out.println("Hang Xe:" +brand);
+        System.out.println("Ten Xe:" +name);
+        System.out.println("Gia Xe:" +price);
     }
 
 }
--- src/session3/Main.java
+++ src/session3/Main.java
@@ -1,19 +1,41 @@
 package session3;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class Main {
     public static void main(String[] args) {
-        Car c = new Car("Audi","A6");
-        System.out.println(c.getBrand());
+//        Car c = new Car("Audi","A6",14);
+//        System.out.println(c.getBrand());
+//
+//        int x = 10;
+//        int[] arr = new int[5];
+//        for(int i=0;i < arr.length;i++){
+//            arr[i] = i+3;
+//        }
+//
+//        Car[] cars = new Car[3];
+//        for(int i=0;i< cars.length;i++){
+//            cars[i] = new Car("Mec","E350",14);
+//        }
 
-        int x = 10;
-        int[] arr = new int[5];
-        for(int i=0;i < arr.length;i++){
-            arr[i] = i+3;
-        }
+        //tao 1 lop car gom: brand, name, price
+        //tao 1 arraylist car, sau do sap xep theo gia tang dan
+        Car[] hs = new Car[5];
+        ArrayList<Car> ls = new ArrayList<>();
+        ls.add(new Car("Mec","MayBach S650",14));
+        ls.add(new Car("Audi","Audi A6",2.5));
+        ls.add(new Car("Lexus","LX570",10));
 
-        Car[] cars = new Car[3];
-        for(int i=0;i< cars.length;i++){
-            cars[i] = new Car("Mec","E350");
-        }
+        Collections.sort(ls, new Comparator<Car>() {
+            @Override
+            public  int compare(Car o1, Car o2) {
+                return o1.getPrice() - o2.getPrice() > 0? 0:-1;
+            }
+        });
+        ls.get(0).inCar();
+        ls.get(1).inCar();
+        ls.get(2).inCar();
     }
 }
