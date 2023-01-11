57c49471d30ca24aa3a20710997ff49f6014d6c6
ChuDucHoangg <110522309+ChuDucHoangg@users.noreply.github.com>
2023-01-11 23:15:08 +0700

    JP1-BUOI-4

---
+++ src/session4/MainPhoneNumber.java
@@ -0,0 +1,32 @@
+package session4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainPhoneNumber {
+    public static void main(String[] args) {
+//        PhoneNumber[] ps = new PhoneNumber[3];
+//        ArrayList<PhoneNumber> ps = new ArrayList<>();
+//        ps.add(new PhoneNumber("Hai",01234567));
+//        ps.add(new PhoneNumber("Duc",02134567));
+//        ps.add(new PhoneNumber("Lam",03124567));
+//        ps.add(new PhoneNumber("Que",04123567));
+//        ps.add(new PhoneNumber("Dung",05123467));
+//        ps.add(new PhoneNumber("Thanh",06123457));
+//        ps.add(new PhoneNumber("Quang",07123456));
+//        ps.add(new PhoneNumber("Lu",07654321));
+//        ps.add(new PhoneNumber("Hoa",07543216));
+//        ps.add(new PhoneNumber("Hau",07432165));
+//
+//        ps.get(0).inDanhSach();
+//        ps.get(1).inDanhSach();
+//        ps.get(2).inDanhSach();
+//        ps.get(3).inDanhSach();
+//        ps.get(4).inDanhSach();
+//        ps.get(5).inDanhSach();
+//        ps.get(6).inDanhSach();
+//        ps.get(7).inDanhSach();
+//        ps.get(8).inDanhSach();
+//        ps.get(9).inDanhSach();
+    }
+}
---
+++ src/session4/PhoneNumber.java
@@ -0,0 +1,50 @@
+package session4;
+
+import java.util.ArrayList;
+
+public class PhoneNumber {
+    private String ten;
+    private ArrayList<String> danhsach = new ArrayList<>();
+
+    public PhoneNumber(){
+    }
+
+    public PhoneNumber(String ten) {
+        this.ten = ten;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public ArrayList<String> getDanhsach() {
+        return danhsach;
+    }
+
+    public void setDanhsach(ArrayList<String> danhsach) {
+        this.danhsach = danhsach;
+    }
+
+    public void addPhone(String danhsachphone){
+        if(!this.getDanhsach().contains(danhsachphone)){
+            danhsach.add(danhsachphone);
+        }
+    }
+
+    public void updatePhone(String olddanhsachphone,String newdanhsachphone){
+        if(danhsach.contains(olddanhsachphone)){
+            int index = danhsach.indexOf(olddanhsachphone);
+            danhsach.add(index,newdanhsachphone);
+        }
+    }
+
+    public void deletePhone(String danhsachphone){
+        if(danhsach.contains(danhsachphone)){
+            danhsach.remove(danhsachphone);
+        }
+    }
+}
