54f1bc2b94f3a09bb08c3a6086e49f1ce5184f2f
ChuDucHoangg <110522309+ChuDucHoangg@users.noreply.github.com>
2023-02-08 15:14:54 +0700

    Practical Exam JP1

---
+++ src/practicalexam/BookDemo.java
@@ -0,0 +1,9 @@
+package practicalexam;
+
+public class BookDemo {
+    public static void main(String[] args) {
+        Book b = new Book("Developing Java Software","Russel Winderand",79.75);
+//        Author a = new Author("Russel","Winderand");
+        System.out.println(b);
+    }
+}
